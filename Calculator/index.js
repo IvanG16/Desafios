dede38d96c7a1b8a9b69a9db6b8c71390d2ffcac
IvanG16 <ivanpintos877@gmail.com>
2023-07-05 17:15:12 -0300

    calc-8 adding js to recognize inputs

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,6 +1,5 @@
 function addEventListenerToButton(element){
     element.addEventListener('click', function(){
-        console.log(element.getAttribute("data-button-type"))
         console.log(element.getAttribute("data-button-value"))
     })
 }
