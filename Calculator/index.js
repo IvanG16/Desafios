5da4f296a58c62ebe692d346f17a23c7bece7d36
IvanG16 <ivanpintos877@gmail.com>
2023-08-15 16:11:32 -0300

    Added revision of model and working functions.

--- Calculator/index.js
+++ Calculator/index.js
@@ -10,76 +10,95 @@ function addEventListenerToButton(element){
                 document.getElementById("old-inputs").innerHTML = '';
                 document.getElementById("inputs").innerHTML = '';
             break;
-            case "number":
 
-                current += value;
-
-                document.getElementById("inputs").innerHTML = current;
+            case "delete":
+                del();
             break;
+            
+
             case "operator":
 
-                if(operator!=undefined){
-                    operator = value;
-                    
-                }else{
-                    document.getElementById("inputs").innerHTML = '';
-                    if(current!=''){
-                        numbers.push(current);
-                        operator = value;
-                        document.getElementById("old-inputs").innerHTML = numbers[0] + operator;
-                        
-                    }
+                numbers[1]=value;
+                if(numbers.length > 3){
+                    operation(numbers);
                 }
-                current='';
+                parenthesis(numbers);
+
             break;
+
+            case "number":
+
+                numbers[1] ? numbers[2]+=value : numbers[0]+=value;
+                parenthesis(numbers);
+            break;
+            
             case "equal":
 
-                if(current!=''){
-                    numbers.push(current);
-                    document.getElementById("old-inputs").innerHTML = numbers[0] + operator + numbers[1];
-                    document.getElementById("inputs").innerHTML = operation(numbers, operator);
-                }
-                numbers.splice(1, 1);
-                current=''
+                parenthesis(numbers);
+                operation(numbers);
+                document.getElementById("inputs").innerHTML = numbers[0];
+                numbers[2]='';
+                
             break;
 
+            case "number-sign":
+                if(numbers[1]){
+                    numbers[2]= numbers[2]* -1;
+                    document.getElementById("inputs").innerHTML = '('+numbers[2]+')';
+                }else{
+                    numbers[0]= numbers[0]* -1;
+                    document.getElementById("inputs").innerHTML = numbers[0];
+                }
+            break;
         }
-        
-        console.log(numbers);
-        console.log(operator);
     })
 }
 
-function operation(numbers, operator){
-    if(numbers[1] != undefined){
-        switch(operator){
-            case '+':
-                numbers[0]=Number(numbers[0]) + Number(numbers[1]);
-            break;
-            case '-':
-                numbers[0]=numbers[0] - numbers[1];
-            break;
-            case 'x':
-                numbers[0]=numbers[0] * numbers[1];
-            break;
-            case '/':
-                numbers[0]=numbers[0] / numbers[1];
-            break;
-        }
-        numbers.splice(1, 1);
-        return numbers[0];
+function operation(numbers){
+
+    switch(numbers[1]){
+
+        case "+":
+            numbers[0]= Number(numbers[0]) + Number(numbers[2]);
+        break;
+
+        case "-":
+            numbers[0]= numbers[0] - numbers[2];
+        break;
+
+        case "x":
+            numbers[0]= numbers[0] * numbers[2];
+        break;
+
+        case "/":
+            numbers[0]= numbers[0] / numbers[2];
+        break;
+
     }
 }
+
 function clear() {
     value='';
-    current='';
-    operator=undefined; 
-    numbers =[];
+    numbers =['','',''];
+    
 }
-let current='';
-let numbers =[];
-let operator;
 
+function del(){
+    numbers[0]= numbers[0].toString();
+    numbers[0]= numbers[0].slice(0, -1);
+    document.getElementById("old-inputs").innerHTML = numbers[0];
+    document.getElementById("inputs").innerHTML = '';
+}
+
+function parenthesis(){
+    if(numbers[2].toString().includes('-')){
+        document.getElementById("old-inputs").innerHTML = numbers[0] + numbers[1] + '('+numbers[2]+')';
+    }else{
+        document.getElementById("old-inputs").innerHTML = numbers[0] + numbers[1] + numbers[2];
+    }
+}
+
+let numbers=['','',''];
 
 const buttons = document.getElementsByClassName("calc-button");
 
